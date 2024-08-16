1b846ed3cb959d5dc2f0e9f99cadeb52a615df5a
CodeLordGh <babohkossi228@icloud.com>
2024-08-16 15:24:21 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import LandingPage from './Pages/Landing.page'
+import GsapPage from './Pages/gsap.page'
+
+const App = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
+
+
---
+++ src/Pages/Landing.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const LandingPage = () => {
+
+
+  return (
+    <div className='flex items-center justify-center w-[100vw] h-[100vh]'>
+      <h1 className='font-bold text-5xl text-center '>We are under developement</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
